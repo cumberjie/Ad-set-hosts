0339583f9ba6fb704561273db36fb36f9c0a6f2f
cumberjie <26396320+cumberjie@users.noreply.github.com>
2022-01-23 15:30:24 +0800

    Update update_action.sh

--- update_action.sh
+++ update_action.sh
@@ -21,12 +21,7 @@ curl -s https://gitee.com/qiusunshine233/hikerView/raw/master/ad_v2.txt >> $t
 sed -i '/\(\/\|@\|\*\|^\.\|\:\)/d;s/^/127.0.0.1 /g' $t && echo "海阔影视 hosts 导入成功"
 
 while read i;do curl -s "$i">>$t&&echo "下载成功"||echo "$i 下载失败";done<<EOF
-# https://raw.githubusercontent.com/E7KMbb/AD-hosts/master/system/etc/hosts
-https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts
-# https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
-https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts
-https://raw.githubusercontent.com/ilpl/ad-hosts/master/hosts
-https://gitee.com/lhzgl6587/hosts/raw/master/hosts
+https://anti-ad.net/easylist.txt
 EOF
 
 # Github520 hosts
@@ -38,13 +33,14 @@ dos2unix */*
 
 # 保留必要 host
 # 只保留 127、0 开头的行
-sed -i "/^\s*\(127\|0\)/!d" $t
+sed -i "/^\s*\(||\)/!d" $t
 # 删除空白符和 # 及后
 sed -i "s/\s\|#.*//g" $t
 # 删除 127.0.0.1 、 0.0.0.0 、 空行、第一行
-sed -i "s/^\(127.0.0.1\|0.0.0.0\)//g" $t
+sed -i "s/^\(127.0.0.1\|0.0.0.0\|'^'\)//g" $t
 # 删除 . 或 * 开头的
 sed -i "/^\.\|^\*/d" $t
+sed -i "/*/d" $t
 
 # 使用声明
 statement="# $(date '+%Y-%m-%d %T')\n# 小贝塔自用，请勿商用\n\n"
